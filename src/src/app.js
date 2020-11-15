e0be542acd9439d6e9c358f18e34df680b8213a7
Jacob Adams <jacobladams@gmail.com>
2020-11-15 09:24:44 -0600

    Rename buttons

--- src/src/app.js
+++ src/src/app.js
@@ -149,7 +149,7 @@ export default function App() {
                       isRunning && "hidden"
                     } inline-flex items-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150`}
                   >
-                    Start Recognizer
+                    Start Koko
                   </button>
                 </span>
 
@@ -161,7 +161,7 @@ export default function App() {
                       !isRunning && "hidden"
                     } inline-flex items-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150`}
                   >
-                    Stop Recognizer
+                    Stop Koko
                   </button>
                 </span>
               </div>
