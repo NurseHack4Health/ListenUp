214551eccdd6233e65871ca0adff90d0069c111d
Jacob Adams <jacobladams@gmail.com>
2020-11-15 00:10:07 -0600

    Added logos. Fixed stop recognizing. Cleanup.

--- src/src/app.js
+++ src/src/app.js
@@ -114,29 +114,18 @@ export default function App() {
 	const [option, setOption] = useState('text');
 	const [phrases, setPhrases] = useState([]);
 	const [isRunning, setIsRunning] = useState(false);
-	const [gifSrc, setGifSrc] = useState(staticAvatar);
-
-	// const debouncedPhrase = useDebounce(phrase, 500);
-
-	let subscriber;
+  const [gifSrc, setGifSrc] = useState(staticAvatar);
+  const [subscriber, setSubscriber] = useState();
 
 	function startRecognizer() {
 		recognizer.startRecognizer();
-		// document.getElementById("status").innerHTML = "Status: running";
-		// recognizer.recognized$.next("hello world");
-		// document.getElementById('stopRecognizer').classList.remove('hidden');
-		// document.getElementById('startRecognizer').classList.add('hidden');
 		setIsRunning(true);
 
-		// const option = document.getElementById("option").value;
 		console.log(option);
 
-		// if (option === "text") {
 		if (option === 'text') {
-			// let gifEl = document.getElementById('gifEl');
-			// gifEl.classList.add('static');
 			setGifSrc(staticAvatar);
-			subscriber = recognizer.recognized$
+			setSubscriber(recognizer.recognized$
 				.pipe(scan((acc, current) => [current].concat(acc), []))
 				.subscribe(
 					(phrases) => {
@@ -149,25 +138,16 @@ export default function App() {
 					() => {
 						console.log('complete');
 					}
-				);
-			// } else if (option === "asl") {
+				));
 		} else if (option === 'asl') {
-			// gifEl.classList.remove('static');
 
-			subscriber = textToAsl(recognizer.recognized$).subscribe(
+			setSubscriber(textToAsl(recognizer.recognized$).subscribe(
 				(data) => {
 					if (data) {
-						// let gifEl = document.getElementById('gifEl');
-						// gifEl.src = `data:image/png;base64, ${data.gif}`;
 						setGifSrc(`data:image/png;base64, ${data.gif}`);
-						// document.getElementById('results').innerHTML =
-						// 	data.word;
 						setPhrases([data.word]);
 					} else {
-						// let gifEl = document.getElementById('gifEl');
-						// gifEl.src = '';
 						setGifSrc(staticAvatar);
-						// document.getElementById('results').innerHTML = '';
 						setPhrases([]);
 					}
 				},
@@ -177,83 +157,22 @@ export default function App() {
 				() => {
 					console.log('complete');
 				}
-			);
+			));
 		}
 	}
 
 	function stopRecognizer() {
-		recognizer.stopRecognizer();
-		subscriber.unsubscribe();
+    recognizer.stopRecognizer();
+    if(subscriber) {
+      subscriber.unsubscribe();
+    }
+		
 		setIsRunning(false);
-		// document.getElementById("status").innerHTML = "Status: stopped";
 	}
-
-	//   const getResult = async () => {
-	//     try {
-	//       if (!phrase) return;
-	//       gifEl.current.src = "https://i.stack.imgur.com/h6viz.gif";
-
-	//       // https://mindrocketsinc.com/api/Dic/en/sarah/Deaf.gif
-	//       const r = await axios.get(
-	//         `https://mindrocketsinc.com/api/getSignedWords.aspx?r=${phrase
-	//           .split(" ")
-	//           .join(
-	//             "+"
-	//           )}&av=sarah&lang=en&icmp=false&cmpcnt=193722&c=10124&u=http%3A%2F%2Fmindrocketsinc.com%2F`
-	//       );
-	//       // console.log(r);
-
-	//       const gifs = await Promise.all(
-	//         r.data.map(async (d) => {
-	//           const response = await axios.get(
-	//             "https://mindrocketsinc.com/api" + d.URL.slice(1, d.URL.length),
-	//             {
-	//               responseType: "arraybuffer"
-	//             }
-	//           );
-	//           // console.log(response)
-	//           return Buffer.from(response.data, "binary").toString("base64");
-	//         })
-	//       );
-
-	//       // for (let j = 0; j < 3; j++) {
-	//       //   for (let i = 0; i < gifs.length; i++) {
-	//       //     await sleep(1000);
-	//       //     gifEl.current.src = `data:image/png;base64, ${gifs[i]}`;
-	//       //     wordEl.current.innerText = r.data[i].NewWord;
-	//       //   }
-	//       // }
-
-	//       window.gifReplaceInterval = setInterval(function () {
-	//         gifEl.current.src = `data:image/png;base64, ${gifs[window.gifI]}`;
-	//         wordEl.current.innerText = r.data[window.gifI].NewWord;
-	//         window.gifI += 1;
-	//         if (window.repeat < 3 && !gifs[window.gifI]) {
-	//           window.gifI = 0;
-	//           window.repeat += 1;
-	//         }
-	//         if (!gifs[window.gifI]) {
-	//           window.gifI = 0;
-	//           clearInterval(window.gifReplaceInterval);
-	//           gifEl.current.src = "";
-	//           wordEl.current.innerText = "";
-	//         }
-	//       }, 1000);
-	//     } catch (err) {
-	//       console.error(err);
-	//     }
-	//   };
-
-	//   useEffect(() => {
-	//     window.gifI = 0;
-	//     window.repeat = 0;
-	//     clearInterval(window.gifReplaceInterval);
-	//     getResult();
-	//   }, [debouncedPhrase]);
 
 	return (
 		<div>
-			<h1>Koko</h1>
+			<h1><img src="logo.png" class="logo" alt="Koko logo"></img>Koko</h1>
 			<div className="row">
 				<div className="avatar-wrapper">
 					<img id="gifEl" src={gifSrc} alt="interpreter avatar" />
